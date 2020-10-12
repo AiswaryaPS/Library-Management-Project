1b2293b3b41aff4fff9ad8d11030949bbc48bebe
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-12 20:49:58 +0530

    Update library.h

--- 03_Implementation/inc/library.h
+++ 03_Implementation/inc/library.h
@@ -1,28 +1,15 @@
-#ifndef LIBRARY_H_INCLUDED
-#define LIBRARY_H_INCLUDED
-//#include<windows.h>
-#include<stdio.h>
-//#include<conio.h>
-#include <stdlib.h>
-#include<string.h>
-#include<ctype.h>
-#define RETURNTIME 15
-//#include<dos.h>
-//#include<time.h>
-//#include<bios.h>
-#endif
-void returnfunc(void);
-void mainmenu(void);
-void addbooks(void);
-void deletebooks(void);
-void editbooks(void);
-void searchbooks(void);
-//void issuebooks(void);
-void viewbooks(void);
-void closeapplication(void);
-int  getdata();
-int  checkid(int);
-int t(void);
-int Password();
-//void issuerecord();
-//void loaderanim();
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "AticleworldLibBookS.bin"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
